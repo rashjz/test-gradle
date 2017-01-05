8337fbc33fb75fcb5f42d127559f7151a2e9533b
RASH\Mobby <rashadjavad@gmail.com>
2017-01-05 13:35:04 +0400

    main and  test dir 2

--- src/test/java/TestResources.java
+++ src/test/java/TestResources.java
@@ -1,5 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by Mobby on 1/5/2017.
  */
 public class TestResources {
+
+    @Test
+    public void getLive() {
+        Assert.assertEquals(1, 1);
+    }
 }
